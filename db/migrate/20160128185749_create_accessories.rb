5ca4e64f18f0da2cb9ec6d49b417df8355e7ff10
psswf1172 <jennifermabella@gmail.com>
2016-01-28 15:41:47 -0500

    accessory migrated

--- db/migrate/20160128185749_create_accessories.rb
+++ db/migrate/20160128185749_create_accessories.rb
@@ -1,7 +1,14 @@
 class CreateAccessories < ActiveRecord::Migration
   def change
     create_table :accessories do |t|
-
+      t.string :name
+      t.string :element
+      t.string :style
+      t.string :color
+      t.string :occasion
+      t.string :season
+      t.boolean :worn
+      
       t.timestamps null: false
     end
   end
