b6b93d1ffbae5876d04d94b2c0044cc4d3daddaf
mathens <athens_maisie@yahoo.com>
2016-01-28 16:24:03 -0500

    worked on garments controller, garment views. garment model is complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method: :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(seesion[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,8 @@ class UsersController < ApplicationController
   end
 
   private
-
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-  
 end
