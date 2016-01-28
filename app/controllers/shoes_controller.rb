ff01722f3e89cb2ca7aa7a3cee1d147dc6182b47
psswf1172 <jennifermabella@gmail.com>
2016-01-28 15:16:17 -0500

    shoe controller model views not migrated

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,46 @@
 class ShoesController < ApplicationController
+
+  def index
+    @shoes = Shoe.all
+  end
+
+  def show
+    @shoe = Shoe.find(params[:id])
+  end
+
+  def new
+    @shoe = Shoe.new
+  end
+
+  def create
+    @shoe = Shoe.new(shoe_params)
+      if @shoe.save
+        flash[:success] = "Your shoe has been put in your closet!"
+        redirect_to @shoe
+      else
+      render 'new'
+    end
+  end
+
+  def update
+    if @shoe.update(shoe_params)
+      flash[:success] = "Your shoe has been updated!"
+    else
+      render 'new'
+    end
+  end
+
+  def delete
+    @shoe.destroy
+    respond_to do |format|
+      format.html { redirect_to shoes_url}
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def params
+    params.require(:shoe).permit(:name, :element, :style, :color, :occasion, :season, :clean, :last_worn)
+  end
+
 end
--- db/migrate/20160128185722_create_shoes.rb
+++ db/migrate/20160128185722_create_shoes.rb
@@ -1,6 +1,13 @@
 class CreateShoes < ActiveRecord::Migration
   def change
     create_table :shoes do |t|
+      t.string :name
+      t.string :element
+      t.string :style
+      t.string :color
+      t.string :occasion
+      t.string :season
+      t.boolean :worn
 
       t.timestamps null: false
     end
