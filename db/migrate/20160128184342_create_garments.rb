dd7ec093c8381c95b57e35135d042b93862d11c3
mathens <athens_maisie@yahoo.com>
2016-01-28 18:09:55 -0500

    re-added garment controller, views, model, and migration (did not migrate)

--- db/migrate/20160128184342_create_garments.rb
+++ db/migrate/20160128184342_create_garments.rb
@@ -1,8 +1,13 @@
 class CreateGarments < ActiveRecord::Migration
   def change
     create_table :garments do |t|
-
+      t.string :name
+      t.string :element
+      t.string :style
+      t.string :color
+      t.string :occassion
+      t.string :season
+      t.boolean :worn
       t.timestamps null: false
     end
   end
-end
