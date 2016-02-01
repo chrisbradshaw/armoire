4069ab31a917324a455474eaf80031b255b1031b
mathens <athens_maisie@yahoo.com>
2016-02-01 14:32:48 -0500

    fixed migration issue 'index_users_on_email'

--- db/migrate/20160131025720_devise_create_users.rb
+++ db/migrate/20160131025720_devise_create_users.rb
@@ -34,10 +34,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    # add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     add_index :users, :unlock_token,         unique: true
   end
 end
-
