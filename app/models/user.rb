714a720ac030ff7ce4bc3711be6529e43ed66ef0
chrisbradshaw3 <christopherbradshaw11@gmail.com>
2016-02-01 13:51:27 -0500

    not saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :shoes
   has_many :garments
   has_many :accessories
+  has_many :outfits
 
  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
