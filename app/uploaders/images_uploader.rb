1603b0c09d2de20eef402e519e9471fcfb36d7a5
cmc33 <cmcguigan33@gmail.com>
2016-02-02 16:21:51 -0500

    wamp wamp

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -42,7 +42,7 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # end
 
   version :thumb do
-    # process :resize_to_fill => [500, 500]
+    process :resize_to_fill => [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
