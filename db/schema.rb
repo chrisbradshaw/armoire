6aeab4868b5b5d5d83d39f47ef60a45df69d6115
mathens <athens_maisie@yahoo.com>
2016-02-03 14:12:22 -0500

    replaced name links with image links and resized images

--- db/schema.rb
+++ db/schema.rb
@@ -102,7 +102,6 @@ ActiveRecord::Schema.define(version: 20160203153308) do
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
