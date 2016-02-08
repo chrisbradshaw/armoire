90eaaf9da399d14a1ac8d5928f2124b72203f1c5
chrisbradshaw3 <christopherbradshaw11@gmail.com>
2016-02-08 08:22:05 -0500

    fixed geolocation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,20 @@ class HomeController < ApplicationController
   
    FORECAST_KEY = "52257016e380daa78bdba4b79225ab6e"
 def index
+  if current_user
   set_temperature
+end
   render 'index'
 end  
 
 def set_temperature
-   long = current_user.geocode.last
-   lat = current_user.geocode.first
+
+  geocode = current_user.geocode
+  lat, long = geocode.first, geocode.last
    url = "https://api.forecast.io/forecast/#{FORECAST_KEY}/#{long},#{lat}"
    results = JSON.parse(open(url).read)
    @temperature = results['currently']['temperature']
    @summary = results['currently']['summary']
- 
  end
 
   def help
